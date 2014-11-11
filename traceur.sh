a52ebdfa029d84d85f92a8ef72ad60aab7164a33
Jonathan Lipps <jlipps@gmail.com>
2014-11-11 14:01:47 -0800

    allow us to run ./traceur.sh -m to only transpile modified files

--- traceur.sh
+++ traceur.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+set -e
+
+only_modified=false
 traceur=$(npm bin)/traceur
+
+while test $# != 0
+do
+    case "$1" in
+        "--modified") only_modified=true;;
+        "-m") only_modified=true;;
+    esac
+
+    shift
+done
 
 function transpile {
   in=$1
@@ -23,18 +36,43 @@ function transpile {
   do
     sub="$in/"
     file=${file#$sub}
+    run_traceur $in $out $file
+  done
+}
+
+function run_traceur {
+    in=$1
+    out=$2
+    file=$3
     $traceur \
       --out $out/$file \
       --script $in/$file \
       --async-functions \
-      --array-comprehension \
-      --types true \
-      --type-assertions \
-      --type-assertion-module ../node_modules/rtts-assert/src/assert \
-      --annotations
+      --array-comprehension
     perl -pi -e '!$x && s|^("use strict";)?|"use strict";\nrequire("traceur/bin/traceur-runtime");\n| && ($x=1)' $out/$file
-  done
 }
 
-transpile lib lib_es5
-transpile bin bin_es5
+function transpile_files {
+    in=$1
+    out=$2
+    mod_files="`$(npm bin)/gitmodified -p \"$1/\" -e \".js\"`"
+    echo "* Running traceur on modified files only from $in to $out"
+    mkdir -p $out
+    if [[ $mod_files == "" ]]; then
+        echo "  * No modified files in $in"
+    else
+        for file in $mod_files
+        do
+            echo "  * Transpiling $in/$file to $out/$file..."
+            run_traceur $in $out $file
+        done
+    fi
+}
+
+if $only_modified ; then
+    transpile_files lib lib_es5
+    transpile_files bin bin_es5
+else
+    transpile lib lib_es5
+    transpile bin bin_es5
+fi
